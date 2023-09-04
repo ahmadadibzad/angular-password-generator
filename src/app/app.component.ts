719be4a72d3cecca16dff694df94b7dcc235c0a6
ahmadadibzad <a.adibzad@gmail.com>
2023-09-04 22:23:17 +0330

    Add some events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+onChangeLength(event: any){
+  const parsedValue = parseInt(event.target.value);
+
+  if (!isNaN(parsedValue)) {
+    this.length = parsedValue;
+  }
+}
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
 
   onButtonClick() {
     this.password = 'My Passowrd!';
