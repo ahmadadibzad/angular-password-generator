60b875a99869de8245476630c5e51c5843b5e263
ahmadadibzad <a.adibzad@gmail.com>
2023-09-06 23:57:39 +0330

    Add password generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,6 +33,28 @@ onChangeLength(event: any){
   }
 
   onButtonClick() {
-    this.password = 'My Passowrd!';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()';
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword = validChars[index];
+    }
+
+    this.password = generatedPassword;
   }
 }
